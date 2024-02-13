7008986b4c96b78dd548b9f2985736fb88f8ff13
--get <3050155211@qq.com>
2024-02-13 13:46:18 +0800

    不知道，抄的百度

--- log.js
+++
@@ -1,9 +0,0 @@
-export default (t = "") => {
-    const date = new Date()
-    const hour = `${date.getHours()}`.padStart(2, "0")
-    const minute = `${date.getMinutes()}`.padStart(2, "0")
-    const second = `${date.getSeconds()}`.padStart(2, "0")
-
-    const time = `${hour}:${minute}:${second}`
-    console.log(`| ${time} |  ${t}`)
-}
