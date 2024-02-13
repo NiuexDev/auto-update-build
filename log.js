9638a5a068a8ee31e19420e8a5269897a027efd2
--get <3050155211@qq.com>
2024-02-13 12:51:34 +0800

    修复了时分秒补全的漏洞

--- log.js
+++ log.js
@@ -1,6 +1,6 @@
 export default (t) => {
     const date = new Date()
-    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds().padStart(2, "0")}`
+    const time = `${date.getHours()}:${date.getMinutes()}:${`${date.getSeconds()}`.padStart(2, "0")}`
     time.padEnd(10)
     console.log(`| ${time} |  ${t}`)
 }
