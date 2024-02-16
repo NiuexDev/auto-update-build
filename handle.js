1c59616b09da0677162addd27a99699dc6359dbd
--get <3050155211@qq.com>
2024-02-16 19:39:06 +0800

    增加了try捕获错误

--- handle.js
+++ handle.js
@@ -32,59 +32,69 @@ async function runCommand(name) {
     log( `-------${`正在更新：${name}`.padEnd(54, "-")}` )
     log()
 
+
+    try {
+        
+
     // 进入网站目录
-    process.chdir(path)
-    log(`切换工作目录为：${process.cwd()}`)
+        process.chdir(path)
+        log(`切换工作目录为：${process.cwd()}`)
 
-/**
- * Build
- */
+    /**
+     * Build
+     */
 
-    // 判断目录是否存在，存在则删除
-    if ( existsSync("./build/") ) {
-        log(`清除build目录`)
-        execSync("rm -rf ./build/", {stdio: 'inherit'})
+        // 判断目录是否存在，存在则删除
+        if ( existsSync("./build/") ) {
+            log(`清除build目录`)
+            execSync("rm -rf ./build/", {stdio: 'inherit'})
+        }
+
+        // git clone
+        log("git clone...")
+        log()
+        execSync(`git clone ${repository} build`, {stdio: 'inherit'})
+        log()
+        log("\ngit clone 完成")
+        
+        // 进入build目录
+        process.chdir("./build/")
+        log(`进入build目录：${process.cwd()}`)
+
+
+        // 运行命令
+        commands.forEach(command => {
+            log(`运行 ${command}`)
+            log()
+            execSync(command, {stdio: 'inherit'})
+            log()
+            log("完成")
+        })
+
+    /**
+     * 部署
+     */
+
+        // 重命名原online文件夹
+        if ( existsSync("../online/") ) {
+            log("重命名online")
+            renameSync("../online/", "../old_online/")
+        }
+        // 输出到online
+        log(`移动输出到online`)
+        renameSync(`./${output}`, "../online/")
+        
+        // 删除old_online
+        log("删除old_online")
+        execSync("rm -rf ../old_online/")
+
+        
+    } catch {
+        log()
+        log( `-------${"更新失败".padEnd(54, "-")}` )
+    } finally {
+        log()
+        log( `-------${"更新完成".padEnd(54, "-")}` )
     }
 
-    // git clone
-    log("git clone...")
-    log()
-    execSync(`git clone ${repository} build`, {stdio: 'inherit'})
-    log()
-    log("\ngit clone 完成")
-    
-    // 进入build目录
-    process.chdir("./build/")
-    log(`进入build目录：${process.cwd()}`)
-
-
-    // 运行命令
-    commands.forEach(command => {
-        log(`运行 ${command}`)
-        log()
-        execSync(command, {stdio: 'inherit'})
-        log()
-        log("完成")
-    })
-
-/**
- * 部署
- */
-
-    // 重命名原online文件夹
-    if ( existsSync("../online/") ) {
-        log("重命名online")
-        renameSync("../online/", "../old_online/")
-    }
-    // 输出到online
-    log(`移动输出到online`)
-    renameSync(`./${output}`, "../online/")
-    
-    // 删除old_online
-    log("删除old_online")
-    execSync("rm -rf ../old_online/")
-
-
-    log()
-    log( `-------${"更新完成".padEnd(54, "-")}` )
 }
